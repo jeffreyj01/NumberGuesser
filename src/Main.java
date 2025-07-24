44db374d67afc3f0ca0fc9588166d1be9a3c8712
jeffreyj01 <jeffreyj0112@gmail.com>
2025-07-24 13:56:40 -0600

    Works but needs error handling

--- src/Main.java
+++ src/Main.java
@@ -17,34 +17,34 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Welcome to NumberGuesser");
-        System.out.println("A Random Number has been generated. You have 5 attempts " +
-                "to guess the number. Each time you guess, you'll be told that the " +
-                "number is higher or lower. Good Luck!");
+        System.out.println("A Random Number has been generated between 1 and 100. You have 5 attempts " +
+                "to guess the number. ");
 
         int randomNumber = new Random().nextInt(100);
-        int maxTry = 0;
+        int maxTry = 5;
 
         Scanner input = new Scanner(System.in);
 
-        while(maxTry < 5)
+        for(int i = 0; i < maxTry; i++)
         {
-        if(input.nextInt() > randomNumber)
-        {
-            System.out.println("Your number is higher than the Random Number");
-            input.nextLine();
-            maxTry++;
-        }
-        else if(input.nextInt() < randomNumber)
-        {
-            System.out.println("Your number is less than the Random Number");
-            input.nextLine();
-            maxTry++;
-        }
-        else {
-            System.out.println("You chose the correct Random Number");
-        }
+            System.out.println("Enter your guess...");
+            int guess = input.nextInt();
+            if(guess == randomNumber)
+            {
+                System.out.println("Congratulations! You chose the correct Random Number");
+                break;
+            }
+            else if(guess > randomNumber)
+            {
+                System.out.println("Your number is higher than the Random Number");
+            }
+            else
+            {
+                System.out.println("Your number is less than the Random Number");
+            }
         }
 
         System.out.println("You ran out of guesses. The correct number was " + randomNumber);
+        input.close();
     }
 }
